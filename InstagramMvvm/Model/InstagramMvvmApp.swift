a613858b836f0970cdb62e35eccfbd5b9273cb10
jin-kirino <jinkirino@gmail.com>
2022-06-25 09:53:14 +0900

    firstPush

---
+++ InstagramMvvm/Model/InstagramMvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramMvvmApp.swift
+//  InstagramMvvm
+//
+//  Created by 神　樹里乃 on 2022/06/25.
+//
+
+import SwiftUI
+
+@main
+struct InstagramMvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
