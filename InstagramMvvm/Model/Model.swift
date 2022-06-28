f5c64bc878b24e2883da30cdafcdf4d16d55cb0e
jin-kirino <jinkirino@gmail.com>
2022-06-29 08:09:36 +0900

    Modelのimport SwiftUIを削除

--- InstagramMvvm/Model/Model.swift
+++ InstagramMvvm/Model/Model.swift
@@ -7,17 +7,17 @@
 
 import Foundation
 
-import SwiftUI
 // 配列
 // Modelはデータを扱う責務、範囲
 // ３つの投稿をModelで管理
 struct UserData2: Identifiable {
     let id = UUID()
+    let numberOfLikes:Int
     let userImageName: String
     let userName: String
     let postImageName: String
 }
 
-let jenny = UserData2(userImageName: "jenny_image", userName: "jenny", postImageName: "post_1")
-let zuck = UserData2(userImageName: "zuck_image", userName: "zuck", postImageName: "post_2")
-let john = UserData2(userImageName: "john_image", userName: "john", postImageName: "post_3")
+let jenny = UserData2(numberOfLikes: 200, userImageName: "jenny_image", userName: "jenny", postImageName: "post_1")
+let zuck = UserData2(numberOfLikes: 200, userImageName: "zuck_image", userName: "zuck", postImageName: "post_2")
+let john = UserData2(numberOfLikes: 200, userImageName: "john_image", userName: "john", postImageName: "post_3")
