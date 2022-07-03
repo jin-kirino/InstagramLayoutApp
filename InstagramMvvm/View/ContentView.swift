54fc65d066d1ef84677d2ed601f73184c42f9c72
jinkirino <jinkirino@gmail.com>
2022-07-03 15:08:38 +0900

    TimelineRowViewを作成

--- InstagramMvvm/View/ContentView.swift
+++ InstagramMvvm/View/ContentView.swift
@@ -15,62 +15,7 @@ struct ContentView: View {
     
     var body: some View {
         List(dataList.model) { user in
-            VStack {
-                HStack {
-                    // 投稿者のアイコン画像名
-                    Image(user.userImageName)
-                    // リサイズを指定
-                        .resizable()
-                        .frame(width: 70.0, height: 70.0, alignment: .topLeading)
-                        .padding(.leading, 20.0)
-                        .padding(.bottom, 20.0)
-                        .padding(.top, 10.0)
-                    // 投稿者の名前
-                    Text(user.userName)
-                        .frame(maxWidth: .infinity, alignment: .topLeading)
-                }// HStack
-                // 投稿した画像名
-                Image(user.postImageName)
-                // リサイズを指定
-                    .resizable()
-                    .frame(width: 270.0, height: 270.0, alignment: .center)
-                    .padding(.bottom, 23.0)
-                HStack {
-                    // ハート
-                    Button {
-                        return
-                    } label: {
-                        Image(systemName: "heart")
-                            .resizable()
-                            .frame(width: 20.0, height: 20.0, alignment: .bottomLeading)
-                    }
-                    .padding(.trailing, 10.0)
-                    // コメント
-                    Button {
-                        return
-                    } label: {
-                        Image(systemName: "message")
-                            .resizable()
-                            .frame(width: 20.0, height: 20.0, alignment: .bottomLeading)
-                    }
-                    .padding(.trailing, 10.0)
-                    // 共有
-                    Button {
-                        return
-                    } label: {
-                        Image(systemName: "square.and.arrow.up")
-                            .resizable()
-                            .frame(width: 20.0, height: 20.0, alignment: .bottomLeading)
-                    }
-                } // HStack
-                .padding(.bottom, 23.0)
-                .padding(.leading, 40.0)
-                .frame(maxWidth: .infinity, alignment: .bottomLeading)
-                // いいねの数
-                Text("200likes")
-                    .frame(maxWidth: .infinity, alignment: .topLeading)
-                    .padding(.leading, 40.0)
-            }// VStack
+            TimelineRowView()
         }// List
     }// body
 }// ContentView
