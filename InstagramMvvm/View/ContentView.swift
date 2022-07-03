670e9fadf68b11ac59063a91fe46c4f74f0cd6e3
jin-kirino <jinkirino@gmail.com>
2022-07-03 10:54:49 +0900

    Listを追加

--- InstagramMvvm/View/ContentView.swift
+++ InstagramMvvm/View/ContentView.swift
@@ -12,63 +12,65 @@ struct ContentView: View {
     // @StateObjectをつけてObservableObjectプロトコルのついてるViewModelからの情報を受け取る
     // ObservableObjectプロトコルに準拠したUserDataクラスをViewで共有するため
     @ObservedObject var dataList = ViewModel()
-
+    
     var body: some View {
-        VStack {
-            HStack {
-                // 投稿者のアイコン画像名
-                Image(dataList.model.userImageName)
+        List(dataList) { user in
+            VStack {
+                HStack {
+                    // 投稿者のアイコン画像名
+                    Image(user.userImageName)
                     // リサイズを指定
-                    .resizable()
-                    .frame(width: 70.0, height: 70.0, alignment: .topLeading)
-                    .padding(.leading, 20.0)
-                    .padding(.bottom, 20.0)
-                    .padding(.top, 10.0)
-                // 投稿者の名前
-                Text(dataList.model.userName)
-            }// HStack
-            // 投稿した画像名
-            Image(dataList.model.postImageName)
+                        .resizable()
+                        .frame(width: 70.0, height: 70.0, alignment: .topLeading)
+                        .padding(.leading, 20.0)
+                        .padding(.bottom, 20.0)
+                        .padding(.top, 10.0)
+                    // 投稿者の名前
+                    Text(user.userName)
+                }// HStack
+                // 投稿した画像名
+                Image(user.postImageName)
                 // リサイズを指定
-                .resizable()
-                .frame(width: 270.0, height: 270.0, alignment: .center)
+                    .resizable()
+                    .frame(width: 270.0, height: 270.0, alignment: .center)
+                    .padding(.bottom, 23.0)
+                HStack {
+                    // ハート
+                    Button {
+                        return
+                    } label: {
+                        Image(systemName: "heart")
+                            .resizable()
+                            .frame(width: 20.0, height: 20.0, alignment: .bottomLeading)
+                    }
+                    .padding(.trailing, 10.0)
+                    // コメント
+                    Button {
+                        return
+                    } label: {
+                        Image(systemName: "message")
+                            .resizable()
+                            .frame(width: 20.0, height: 20.0, alignment: .bottomLeading)
+                    }
+                    .padding(.trailing, 10.0)
+                    // 共有
+                    Button {
+                        return
+                    } label: {
+                        Image(systemName: "square.and.arrow.up")
+                            .resizable()
+                            .frame(width: 20.0, height: 20.0, alignment: .bottomLeading)
+                    }
+                } // HStack
                 .padding(.bottom, 23.0)
-            HStack {
-                // ハート
-                Button {
-                    return
-                } label: {
-                    Image(systemName: "heart")
-                        .resizable()
-                        .frame(width: 20.0, height: 20.0, alignment: .bottomLeading)
-                }
-                .padding(.trailing, 10.0)
-                // コメント
-                Button {
-                    return
-                } label: {
-                    Image(systemName: "message")
-                        .resizable()
-                        .frame(width: 20.0, height: 20.0, alignment: .bottomLeading)
-                }
-                .padding(.trailing, 10.0)
-                // 共有
-                Button {
-                    return
-                } label: {
-                    Image(systemName: "square.and.arrow.up")
-                        .resizable()
-                        .frame(width: 20.0, height: 20.0, alignment: .bottomLeading)
-                }
-            } // HStack
-            .padding(.bottom, 23.0)
-            .padding(.leading, 40.0)
-            .frame(maxWidth: .infinity, alignment: .bottomLeading)
-            // いいねの数
-            Text("200likes")
-                .frame(maxWidth: .infinity, alignment: .topLeading)
                 .padding(.leading, 40.0)
-        }// VStack
+                .frame(maxWidth: .infinity, alignment: .bottomLeading)
+                // いいねの数
+                Text("200likes")
+                    .frame(maxWidth: .infinity, alignment: .topLeading)
+                    .padding(.leading, 40.0)
+            }// VStack
+        }// List
     }// body
 }// ContentView
 
