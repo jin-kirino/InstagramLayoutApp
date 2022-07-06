5218fd498e5efd99b9be38fd85beb57715bd9763
jinkirino <jinkirino@gmail.com>
2022-07-06 12:20:54 +0900

    データ本体を構造体PostData内に移動

--- InstagramMvvm/Model/PostData.swift
+++ InstagramMvvm/Model/PostData.swift
@@ -6,14 +6,14 @@
 //
 
 import Foundation
-g
+
 struct PostData {
     func fetch() -> [Post] {
         // ３人分のデータを配列にして返す
         return [postJenny, postZuck, postJohn]
     }// fetch
+    private let postJenny: Post = Post(userName: "jenny", userImageName: "jenny_image", postImageName: "post_1")
+    private let postZuck: Post = Post(userName: "zuck", userImageName: "zuck_image",postImageName: "post_2")
+    private let postJohn: Post = Post(userName: "john", userImageName: "john_image", postImageName: "post_3")
 }// PostData
 // 構造体Postのイニシャライザ
-let postJenny: Post = Post(userName: "jenny", userImageName: "jenny_image", postImageName: "post_1")
-let postZuck: Post = Post(userName: "zuck", userImageName: "zuck_image",postImageName: "post_2")
-let postJohn: Post = Post(userName: "john", userImageName: "john_image", postImageName: "post_3")
