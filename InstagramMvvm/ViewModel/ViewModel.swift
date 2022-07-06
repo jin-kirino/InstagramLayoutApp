23d7b62690a115919bed0b5c1ba641fc31d39891
jinkirino <jinkirino@gmail.com>
2022-07-07 08:18:21 +0900

    変数名を変更

--- InstagramMvvm/ViewModel/ViewModel.swift
+++ InstagramMvvm/ViewModel/ViewModel.swift
@@ -17,16 +17,13 @@ import SwiftUI
 // 3件分のデータを管理する
 
 // カスタムクラス内でデータの状態を管理する(ObservableObject)
-class ViewModel: ObservableObject {
+class TimelineViewModel: ObservableObject {
     // ObservableObjectプロトコル内のプロパティをContentViewのbodyに配信する
     // 配列で定義
-    @Published var model: [Model]
+    @Published var postData: [Post]
     
     init() {
-        // ３人分のデータが配列に格納されている
-        self.model = userList
+        self.postData = postData
     }
-}// ViewModel
+}// TimelineViewModel
 // Modelのデータと同じ型にして配列で受け取れるように@Publishdで定義
-
-// リストを作成してViewに返す
