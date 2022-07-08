ff5b31442f561832089fe1e0e4b583e3784d7bb2
jinkirino <jinkirino@gmail.com>
2022-07-08 22:27:18 +0900

    構造体PostData

--- InstagramMvvm/Model/PostData.swift
+++ InstagramMvvm/Model/PostData.swift
@@ -9,15 +9,16 @@ import Foundation
 
 // データ本体を管理
 struct PostData {
+
+    // ストアドプロパティ
     private let postJenny: Post = Post(userName: "jenny", userImageName: "jenny_image", postImageName: "post_1")
     private let postZuck: Post = Post(userName: "zuck", userImageName: "zuck_image",postImageName: "post_2")
     private let postJohn: Post = Post(userName: "john", userImageName: "john_image", postImageName: "post_3")
-    
+    // メソッド
     func fetch() -> [Post] {
         // ３人分のデータを配列にして返す
         return [postJenny, postZuck, postJohn]
     }// fetch
 }// PostData
-// 構造体Postのイニシャライザ
+// インスタンス化
 let postData = PostData()
-
